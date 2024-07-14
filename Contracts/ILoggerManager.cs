1e88f7d43e02eaa253adf1ef39d42785328232e1
Vadim <vadim.ivashko.04@gmail.com>
2024-07-14 22:53:47 +0300

    Revert "#"
    This reverts commit 30e7993c3fc75c174d86fdd7658812cd6d26485c.

--- Contracts/ILoggerManager.cs
+++ Contracts/ILoggerManager.cs
@@ -1,4 +1,10 @@
-﻿namespace Contracts
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
 {
     public interface ILoggerManager
     {
