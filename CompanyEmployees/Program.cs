db75fb77e4cbdc0e38e57d60aa571e16e1ce5c73
Vadim <vadim.ivashko.04@gmail.com>
2024-07-14 22:45:48 +0300

    Добавление Global Error Handling

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,6 +1,7 @@
 using CompanyEmployees.Extensions;
 using Microsoft.AspNetCore.HttpOverrides;
 using NLog;
+using Contracts;
 
 namespace CompanyEmployees
 {
@@ -26,10 +27,13 @@ namespace CompanyEmployees
 
             var app = builder.Build();
 
-            if (app.Environment.IsDevelopment())
-                app.UseDeveloperExceptionPage();
-            else
+            var logger = app.Services.GetRequiredService<ILoggerManager>();
+            app.ConfigureExceptionHandler(logger);
+
+            if(app.Environment.IsProduction())
+            {
                 app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
