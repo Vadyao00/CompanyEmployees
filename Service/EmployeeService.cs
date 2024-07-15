b753e62ed1c4bae41a10d77d9c6661eff7b8870a
Vadim <vadim.ivashko.04@gmail.com>
2024-07-15 12:43:39 +0300

    Реализация GET запроса на получение employees и employee

--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using Contracts;
+using Entities.Exceptions;
+using Entities.Models;
 using Service.Contracts;
+using Shared.DataTransferObjects;
 
 namespace Service
 {
@@ -15,6 +18,33 @@ namespace Service
             _repositoryManager = repository;
             _logger = logger;
             _mapper = mapper;
+        }
+
+        public IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges)
+        {
+            var company = _repositoryManager.Company.GetCompany(companyId, trackChanges);
+            if(company is null)
+                throw new CompanyNotFoundException(companyId);
+
+            var employeesFromDb = _repositoryManager.Employee.GetEmployees(companyId, trackChanges);
+            var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesFromDb);
+
+            return employeesDto;
+        }
+
+        public EmployeeDto GetEmployee(Guid companyId, Guid id, bool trackChanges)
+        {
+            var company = _repositoryManager.Company.GetCompany(companyId,trackChanges);
+            if(company is null)
+                throw new CompanyNotFoundException(companyId);
+
+            var employeeDb = _repositoryManager.Employee.GetEmployee(companyId, id, trackChanges);
+            if(employeeDb is null)
+                throw new EmployeeNotFoundException(id);
+
+            var employeeDto = _mapper.Map<EmployeeDto>(employeeDb);
+
+            return employeeDto;
         }
     }
 }
