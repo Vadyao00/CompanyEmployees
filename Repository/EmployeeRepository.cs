e096055103485b5d21dd3213911d135ff637af09
Vadim <vadim.ivashko.04@gmail.com>
2024-07-24 12:37:28 +0300

    Добавлены фильтры для возраста работника

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -13,7 +13,7 @@ namespace Repository
 
         public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
         {
-            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId) && (e.Age >= employeeParameters.MinAge && e.Age <= employeeParameters.MaxAge), trackChanges)
                 .OrderBy(e => e.Name)
                 .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
                 .Take(employeeParameters.PageSize)
@@ -26,7 +26,7 @@ namespace Repository
 
         public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) =>
             await FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges)
-                .SingleOrDefaultAsync()!;
+                .SingleOrDefaultAsync();
 
         public void CreateEmployeeForCompany(Guid companyId,Employee employee)
         {
